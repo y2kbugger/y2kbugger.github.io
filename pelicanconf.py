236506683220443a8a7ce9d6de9ab84eaf1a79f5
y2k <git@y2kbugger.com>
2017-12-19 20:16:53 -0500

    modifying settings

--- pelicanconf.py
+++ pelicanconf.py
@@ -7,8 +7,10 @@ SITENAME = 'y2kbugger'
 SITEURL = ''
 
 PATH = 'content'
+THEME = 'themes/william-pelican/'
 
 TIMEZONE = 'America/New_York'
+DEFAULT_DATE_FORMAT = '%Y.%m.%d @ %H:%M:%S'
 
 DEFAULT_LANG = 'en'
 
@@ -18,6 +20,14 @@ CATEGORY_FEED_ATOM = None
 TRANSLATION_FEED_ATOM = None
 AUTHOR_FEED_ATOM = None
 AUTHOR_FEED_RSS = None
+
+MENUITEMS = [
+    ("y2kbugger", '/index.html'),
+    ('Tags', '/tags.html'),
+    # ('About', '/pages/about.html'),
+]
+DISPLAY_PAGES_ON_MENU = True
+DISPLAY_CATEGORIES_ON_MENU = True
 
 # Blogroll
 LINKS = (('Pelican', 'http://getpelican.com/'),
@@ -34,5 +44,20 @@ DEFAULT_PAGINATION = False
 # Uncomment following line if you want document-relative URLs when developing
 #RELATIVE_URLS = True
 
-STATIC_PATHS = ['images', 'extra/CNAME']
-EXTRA_PATH_METADATA = {'extra/CNAME': {'path': 'CNAME'},}
+STATIC_PATHS = [
+    'extra/favicon.ico',
+    'images', 'extra/CNAME',
+]
+
+EXTRA_PATH_METADATA = {
+    'extra/favicon.ico': {'path': 'favicon.ico'},
+    'extra/CNAME': {'path': 'CNAME'},
+}
+
+HEAD_EXTRA = u"""
+    <link rel="icon" href="/favicon.ico" sizes="16x16 32x32 48x48 64x64" type="image/vnd.microsoft.icon">
+"""
+
+COPYRIGHT = """zak kohler 2017 &mdash; Happy Hacking"""
+
+# ENABLE_SLICKNAV = True
