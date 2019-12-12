2023a1db8c5fd5c3c04a36cb9d5b00693b13e41f
y2k <git@y2kbugger.com>
2019-12-11 22:22:35 -0500

    Update slug placeholder

--- publishconf.py
+++ publishconf.py
@@ -15,7 +15,7 @@ SITEURL = 'http://blog.y2kbugger.com'
 RELATIVE_URLS = False
 
 FEED_ALL_ATOM = 'feeds/all.atom.xml'
-CATEGORY_FEED_ATOM = 'feeds/%s.atom.xml'
+CATEGORY_FEED_ATOM = 'feeds/{slug}.atom.xml'
 
 DELETE_OUTPUT_DIRECTORY = True
 
